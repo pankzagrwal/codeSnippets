a7ea495c923e0fef6842e807e722386909806cc2
Pankaj <pankzmail@gmail.com>
2016-02-27 14:08:40 +0530

    add toUnderscore method

--- util.js
+++ util.js
@@ -46,3 +46,27 @@ var prime = function (n) {
   
   return ret;
 }
+
+
+// Added method to "String".. to convert camelCase string to camel_case. 
+String.prototype.toUnderscore = function () {
+    var strArr = this.split("");
+    
+    for (var i = 0; i < strArr.length; i++) {
+        if (strArr[i].charCodeAt() >= 65 && strArr[i].charCodeAt() <= 90 ) {
+             strArr[i] = strArr[i].toLowerCase();
+            if (i !== 0) {
+                strArr.splice(i, 0, "_");
+            }
+        }
+        else if (strArr[i].charCodeAt() >= 97 && strArr[i].charCodeAt() <= 122 ) {
+            
+        }
+        
+        else {
+            strArr.splice(i, 1, "_")
+        }
+    }
+    
+    return strArr.join("");
+}
